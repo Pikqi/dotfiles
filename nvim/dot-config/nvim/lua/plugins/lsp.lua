b9630ec587ffe08c91d4d1e5ff4ca44894b57150
Petar Obradovic <perazmn@gmail.com>
2025-10-09 18:03:17 +0200

    [nvim] 0.11.0 lsp setup and nvim-org

--- nvim/dot-config/nvim/lua/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,194 +1,33 @@
--- local status_ok, null_ls = pcall(require, "null-ls")
--- if not status_ok then
--- 	return
--- end
---
--- null_ls.setup({
--- 	sources = {
--- 		-- null_ls.builtins.formatting.prettier,
--- 		null_ls.builtins.formatting.prettierd,
--- 		-- null_ls.builtins.formatting.stylua,
---
--- 		-- null_ls.builtins.formatting.stylelint,
--- 		-- null_ls.builtins.diagnostics.stylelint,
--- 		--
--- 		-- null_ls.builtins.code_actions.eslint,
--- 		-- null_ls.builtins.diagnostics.eslint,
---
--- 		null_ls.builtins.diagnostics.eslint_d,
--- 		null_ls.builtins.code_actions.eslint_d,
--- 	},
--- 	on_attach,
--- })
-
-
-local on_attach = function(client, bufnr)
-	if client.name == "ts_ls" then
-		client.server_capabilities.document_formatting = false
-	end
-	vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({timeout_ms=3000})]])
-end
-
 return {
-
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			-- ensure_installed =
-		},
+		opts = {},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
 		},
+		-- config = function()
+		-- 	vim.lsp.enable("zls", true)
+		-- end
 	},
 	{
 		"neovim/nvim-lspconfig",
-		dependencies = { "b0o/schemastore.nvim", "saghen/blink.cmp", },
 		config = function()
-			local lspconfig = require("lspconfig")
+			local on_attach = function(client, bufnr)
+				-- if client.name == "ts_ls" then
+				-- 	client.server_capabilities.document_formatting = false
+				-- end
+				vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({timeout_ms=3000})]])
+			end
 
-			local capabilities = require('blink.cmp').get_lsp_capabilities()
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-			lspconfig.arduino_language_server.setup({
+			vim.lsp.config("*", {
 				on_attach = on_attach,
-				capabilities = capabilities,
-			})
+			}
 
-			lspconfig.gopls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-
-			lspconfig.astro.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			lspconfig.tailwindcss.setup({
-				settings = {
-					tailwindCSS = {
-						experimental = {
-							-- classRegex = { "[twMerge,twJoin]\\([^]([^)]*).\\)", "[Cc]lass[Nn]ames \\=([^;]*);" }
-							classRegex = {
-								{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-							},
-						},
-					},
-				},
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.cssmodules_ls.setup({
-				on_attach = on_attach,
-				init_options = {
-					camelCase = false,
-				},
-			})
-
-			lspconfig.ts_ls.setup({
-				on_attach = on_attach,
-				--filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-				cmd = { "typescript-language-server", "--stdio" },
-				capabilities = capabilities,
-			})
-
-			-- lspconfig.eslint.setup {
-			-- 	on_attach = on_attach
-			-- }
-
-			lspconfig.zls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					zls = {
-						enable_autofix = true,
-						enable_build_on_save = true,
-						build_on_save_args = { "test", },
-					},
-				},
-			})
-
-			lspconfig.pylsp.setup({
-				on_attach = on_attach,
-			})
-
-			-- lspconfig.pyright.setup {
-			-- 	on_attach = on_attach
-			-- }
-
-			lspconfig.lua_ls.setup({
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							-- Make the server aware of Neovim runtime files
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-					},
-				},
-			})
-			lspconfig.clangd.setup({
-				on_attach = on_attach,
-			})
-
-			lspconfig.solc.setup({
-				on_attach = on_attach,
-			})
-
-			lspconfig.solc.setup({
-				on_attach = on_attach,
-			})
-
-			lspconfig.jsonls.setup({
-				settings = {
-					json = {
-						schemas = require("schemastore").json.schemas(),
-						validate = { enable = true },
-					},
-				},
-				capabilities = capabilities,
-			})
+			)
+			vim.lsp.enable("zls", true)
 		end
-	},
 
-	-- {
-	-- 	"nvimtools/none-ls.nvim",
-	-- 	config = function()
-	-- 		local null_ls = require("null-ls")
-	-- 		null_ls.setup({
-	-- 			sources = {
-	-- 				-- null_ls.builtins.formatting.prettier,
-	-- 				null_ls.builtins.formatting.prettierd,
-	-- 				-- null_ls.builtins.formatting.stylua,
+	}
 
-	-- 				-- null_ls.builtins.formatting.stylelint,
-	-- 				-- null_ls.builtins.diagnostics.stylelint,
-	-- 				--
-	-- 				-- null_ls.builtins.code_actions.eslint,
-	-- 				-- null_ls.builtins.diagnostics.eslint,
-
-	-- 				null_ls.builtins.diagnostics.eslint_d,
-	-- 				-- null_ls.builtins.code_actions.eslint_d,
-	-- 			},
-	-- 			on_attach,
-	-- 		})
-	-- 	end
-
-
-	-- }
 }
