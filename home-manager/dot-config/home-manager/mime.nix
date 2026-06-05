68971c95e1b795e308e5f885eafe3349b80cb9bc
Petar Obradovic <perazmn@gmail.com>
2026-06-05 20:51:00 +0200

    [nix] format

--- home-manager/dot-config/home-manager/mime.nix
+++ home-manager/dot-config/home-manager/mime.nix
@@ -17,11 +17,11 @@
 { config, pkgs, ... }:
 
 let
-  browser     = "librewolf.desktop";
+  browser = "librewolf.desktop";
   fileManager = "org.kde.dolphin.desktop";
-  textEditor  = "nvim.desktop";
-  pdfViewer   = "org.pwmt.zathura.desktop";
-  imageView   = "imv.desktop";
+  textEditor = "nvim.desktop";
+  pdfViewer = "org.pwmt.zathura.desktop";
+  imageView = "imv.desktop";
   mediaPlayer = "mpv.desktop";
 in
 {
@@ -29,58 +29,59 @@ in
     enable = true;
     defaultApplications = {
       # Browser
-      "x-scheme-handler/http"         = browser;
-      "x-scheme-handler/https"        = browser;
-      "x-scheme-handler/about"        = browser;
-      "x-scheme-handler/unknown"      = browser;
-      "text/html"                     = browser;
-      "application/xhtml+xml"         = browser;
-      "application/x-extension-htm"   = browser;
-      "application/x-extension-html"  = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "x-scheme-handler/about" = browser;
+      "x-scheme-handler/unknown" = browser;
+      "text/html" = browser;
+      "application/xhtml+xml" = browser;
+      "application/x-extension-htm" = browser;
+      "application/x-extension-html" = browser;
 
       # File manager
-      "inode/directory"               = fileManager;
-      "x-scheme-handler/file"         = fileManager;
+      "inode/directory" = fileManager;
+      "x-scheme-handler/file" = fileManager;
 
       # Text editor
-      "text/plain"                    = textEditor;
-      "text/markdown"                 = textEditor;
-      "text/x-c"                      = textEditor;
-      "text/x-c++"                    = textEditor;
-      "text/x-java"                   = textEditor;
-      "text/x-python"                 = textEditor;
-      "text/x-shellscript"            = textEditor;
-      "text/css"                      = textEditor;
-      "application/json"              = textEditor;
-      "application/x-shellscript"     = textEditor;
+      "text/plain" = textEditor;
+      "text/markdown" = textEditor;
+      "text/x-c" = textEditor;
+      "text/x-c++" = textEditor;
+      "text/x-java" = textEditor;
+      "text/x-python" = textEditor;
+      "text/x-shellscript" = textEditor;
+      "text/css" = textEditor;
+      "application/json" = textEditor;
+      "application/x-shellscript" = textEditor;
 
       # PDF viewer
-      "application/pdf"               = pdfViewer;
+      "application/pdf" = pdfViewer;
 
       # Image viewer
-      "image/png"                     = imageView;
-      "image/jpeg"                    = imageView;
-      "image/gif"                     = imageView;
-      "image/webp"                    = imageView;
-      "image/svg+xml"                 = imageView;
-      "image/bmp"                     = imageView;
-      "image/tiff"                    = imageView;
+      "image/png" = imageView;
+      "image/jpeg" = imageView;
+      "image/gif" = imageView;
+      "image/webp" = imageView;
+      "image/svg+xml" = imageView;
+      "image/bmp" = imageView;
+      "image/tiff" = imageView;
 
       # Video player
-      "video/mp4"                     = mediaPlayer;
-      "video/mpeg"                    = mediaPlayer;
-      "video/webm"                    = mediaPlayer;
-      "video/x-matroska"              = mediaPlayer;
-      "video/x-msvideo"               = mediaPlayer;
-      "video/quicktime"               = mediaPlayer;
+      "video/mp4" = mediaPlayer;
+      "video/mpeg" = mediaPlayer;
+      "video/webm" = mediaPlayer;
+      "video/x-matroska" = mediaPlayer;
+      "video/x-msvideo" = mediaPlayer;
+      "video/quicktime" = mediaPlayer;
 
       # Audio player
-      "audio/mpeg"                    = mediaPlayer;
-      "audio/ogg"                     = mediaPlayer;
-      "audio/flac"                    = mediaPlayer;
-      "audio/aac"                     = mediaPlayer;
-      "audio/x-wav"                   = mediaPlayer;
-      "audio/webm"                    = mediaPlayer;
+      "audio/mpeg" = mediaPlayer;
+      "audio/ogg" = mediaPlayer;
+      "audio/flac" = mediaPlayer;
+      "audio/aac" = mediaPlayer;
+      "audio/x-wav" = mediaPlayer;
+      "audio/webm" = mediaPlayer;
     };
   };
 }
+
